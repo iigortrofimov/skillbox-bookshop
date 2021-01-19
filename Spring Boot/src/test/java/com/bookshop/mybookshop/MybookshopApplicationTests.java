c1a1f5a544c9a3af62f4841b882754847246baf5
Igor Trofimov <igor.trofimov@t-systems.com>
2021-01-19 14:12:29 +0300

    init commit for Spring Boot part

---
+++ Spring Boot/src/test/java/com/bookshop/mybookshop/MybookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshop.mybookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
