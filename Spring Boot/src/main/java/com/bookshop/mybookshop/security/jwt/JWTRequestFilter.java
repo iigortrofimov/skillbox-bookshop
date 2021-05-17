af82eaf3ed7898f8fcfa949fadfa688015c5e901
Igor Trofimov <igor.trofimov@t-systems.com>
2021-05-17 14:38:25 +0300

    [Module 8] hometasks | сщÐcorrected remarks

--- Spring Boot/src/main/java/com/bookshop/mybookshop/security/jwt/JWTRequestFilter.java
+++ Spring Boot/src/main/java/com/bookshop/mybookshop/security/jwt/JWTRequestFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JWTRequestFilter extends OncePerRequestFilter {
 
     private final BookStoreUserDetailsService bookStoreUserDetailsService;
@@ -25,28 +27,65 @@ public class JWTRequestFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+                                    FilterChain filterChain) throws IOException, ServletException {
+        BookStoreUserDetails userDetails;
         String token = null;
         String username = null;
         Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    token = cookie.getValue();
-                    username = jwtUtil.extractUsername(token);
-                }
-                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                    BookStoreUserDetails userDetails =
-                            (BookStoreUserDetails) bookStoreUserDetailsService.loadUserByUsername(username);
-                    if (!jwtTokenBlackList.findByToken(token).isPresent() && jwtUtil.validateToken(token, userDetails)) {
-                        UsernamePasswordAuthenticationToken authenticationToken =
-                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                    }
-                }
+
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (cookies == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                token = cookie.getValue();
+                username = jwtUtil.extractUsername(token);
             }
         }
+
+        if (token == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        log.info("Login attempt: Detected JWT Token. Token: {}", token);
+
+        if (jwtTokenBlackList.findByToken(token).isPresent()) {
+            log.info("Token was found in BlackList");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (username == null) {
+            log.info("Token doesn't contain user data");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        userDetails = (BookStoreUserDetails) bookStoreUserDetailsService.loadUserByUsername(username);
+        log.info("User from JWT Token: {} is found", userDetails.getUsername());
+
+        if (!jwtUtil.validateToken(token, userDetails)) {
+            log.info("Invalid token");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        log.info("Authentication's completed with next credentials: Login : {}, Authorities: {}, Ip-address: {}",
+                userDetails.getUsername(), userDetails.getAuthorities(), request.getRemoteAddr());
+
         filterChain.doFilter(request, response);
     }
 }
